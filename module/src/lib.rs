39d8fb38b321fed51d099a21543f127f5eda649b
IgorKhomenko <khomenkoigor+1@gmail.com>
2024-03-06 17:30:03 +0200

    cleanup

--- module/src/lib.rs
+++ module/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod funcs {
+  #[allow(unused_imports)]
+  use wasmedge_bindgen::*;
   use wasmedge_bindgen_macro::*;
-
-  use std::mem;
 
   #[wasmedge_bindgen]
   pub fn say(name: String, age: i32, temp: f32) -> String {
@@ -10,22 +10,5 @@ pub mod funcs {
     println!("temp: {}", temp);
 
     return format!("hello {}, age: {}, temp: {}", name.as_str(), age, temp);
-  }
-
-  #[no_mangle]
-  pub unsafe extern fn allocate(size: i32) -> *const u8 {
-    // println!("allocate size={}", size);
-
-    let buffer = Vec::with_capacity(size as usize);
-
-    let buffer = mem::ManuallyDrop::new(buffer);
-    buffer.as_ptr() as *const u8
-  }
-
-  #[no_mangle]
-  pub unsafe extern fn deallocate(pointer: *mut u8, size: i32) {
-    // println!("deallocate size={}", size);
-
-    drop(Vec::from_raw_parts(pointer, size as usize, size as usize));
   }
 }
